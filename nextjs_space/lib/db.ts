a0dc5d5cf8cceabcf30e9644c9ff3b51758f5fd8
thiagofregolao-blip <thiago.fregolao@gmail.com>
2025-12-19 11:47:09 -0300

    fix: replace Proxy-based Prisma init with standard singleton

--- nextjs_space/lib/db.ts
+++ nextjs_space/lib/db.ts
@@ -4,17 +4,10 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-function getPrismaClient(): PrismaClient {
-  if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = new PrismaClient()
-  }
-  return globalForPrisma.prisma
-}
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['error'],
+  })
 
-// Use a getter to lazily initialize the client
-export const prisma = new Proxy({} as PrismaClient, {
-  get(target, prop) {
-    const client = getPrismaClient()
-    return (client as any)[prop]
-  }
-})
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
